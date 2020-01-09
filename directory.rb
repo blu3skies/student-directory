6330633e2539d8e430a06f1fc85dc1c85c3c762c
Joe Ellingworth <joeellingworth@gmail.com>
2020-01-09 11:54:35 +0000

    refactoring the directory.rb file using array.

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-student_count = 11
 # adding students into array
 students = [
   "Dr. Hannible Lecter",
@@ -17,16 +16,7 @@ students = [
 
 puts "The Sudents of Villans Acadmey" # title
 puts "-----------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
+students.each { |student|
+puts student }
 #printing the totat students
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
