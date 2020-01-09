533fe53ff81de9cfd7aaff517fec6a197dc7a06f
Joe Ellingworth <joeellingworth@gmail.com>
2020-01-09 11:42:49 +0000

    put students into array

--- directory.rb
+++ directory.rb
@@ -1,17 +1,32 @@
 student_count = 11
+# adding students into array
+students = [
+  "Dr. Hannible Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex Delarge",
+  "The Wicked Witch of the west",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+#print them
 
 puts "The Sudents of Villans Acadmey" # title
 puts "-----------------"
-puts "Dr. Hannible Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "michael Corleone"
-puts "Alex Delarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#next is example of of print vs puts
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+#printing the totat students
 puts "Overall, we have #{student_count} great students"
