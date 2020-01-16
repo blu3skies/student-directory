dc851e4de38d0bcb75d2d734a35531c96837096a
Joe Ellingworth <joeellingworth@gmail.com>
2020-01-16 18:09:23 +0000

    copied directory changers: print student/students. replaced .chomp method

--- directory_copy_exercises.rb
+++ directory_copy_exercises.rb
@@ -10,21 +10,24 @@ def input_students
   loop do
     puts "please enter the names of the students"
     puts "To finish, just type 'quit'"
-    name = gets.chomp
+    name = gets.sub!("\n", "")
     if name == "quit"
       break
     end
     puts "input hobby"
-    hobby = gets.chomp
+    hobby = gets.sub!("\n", "")
     puts "input country of birth"
-    birth_place = gets.chomp
+    birth_place = gets.sub!("\n", "")
     puts "input height"
-    height = gets.chomp
+    height = gets.sub!("\n", "")
     puts "input month (default is #{default_month})"
-    month = gets.chomp
+    month = gets.sub!("\n", "")
+
     if month == ""
       month = default_month
     end
+
+
 
     # add the student has into array
     students << {
@@ -46,21 +49,21 @@ def print_header # title
 end
 
 def print(students)
-  counter = students.count
-  #students.each { |student|
-    until counter == 0
-      puts "Student name = #{students[counter-1][:name]} (#{students[counter-1][:cohort]} cohort)"
-      puts "Favorite hobby = #{students[counter-1][:hobby]}"
-      puts "birth place = #{students[counter-1][:birth_place]}"
-      puts "Height = #{students[counter-1][:height]}"
-      puts
-      counter -= 1
+  students.map do |student|
+      puts "Student name = #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "Favorite hobby = #{student[:hobby]}"
+      puts "birth place = #{student[:birth_place]}"
+      puts "Height = #{student[:height]}"
     end
-
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  statement = "We have #{names.count} student"
+  if names.count < 2
+    puts statement
+  else
+    puts statement + "s"
+  end
 end
 
 students = input_students
